8df2a15e3818e7dc1e468d610e483633a7280f37
Clasher3000 <andriy.bakalets2005@gmail.com>
2024-10-14 20:32:38 +0300

    init

---
+++ backend/src/main/java/com/example/demo_case_crm/DemoCaseCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_case_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCaseCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCaseCrmApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/demo_case_crm/DemoCaseCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_case_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCaseCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
