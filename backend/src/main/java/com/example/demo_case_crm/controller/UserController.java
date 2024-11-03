eb9f7fbdf10205378e38975c1263e06e5870cb8f
Clasher3000 <andriy.bakalets2005@gmail.com>
2024-11-03 23:03:17 +0200

    add user controller + bug fix

---
+++ backend/src/main/java/com/example/demo_case_crm/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo_case_crm.controller;
+
+import com.example.demo_case_crm.entity.User;
+import com.example.demo_case_crm.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAll(){
+        return userService.getAll();
+    }
+}
--- backend/src/main/java/com/example/demo_case_crm/entity/User.java
+++ backend/src/main/java/com/example/demo_case_crm/entity/User.java
@@ -1,12 +1,16 @@
 package com.example.demo_case_crm.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@Table(name = "\"user\"")
+
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,7 +20,9 @@ public class User {
     @Column(name="last_name")
     private String lastName;
     private String email;
+    @JsonIgnore
     private String password;
+    @JsonIgnore
     private boolean active;
     @Column(name="created_at")
     private LocalDate createdAt;
