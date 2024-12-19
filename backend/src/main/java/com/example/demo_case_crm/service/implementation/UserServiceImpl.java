69ebbc1ea2e6751df1efd36b6057d5db870d77c9
Clasher3000 <andriy.bakalets2005@gmail.com>
2024-12-20 00:55:49 +0200

    update behavior a bit

--- backend/src/main/java/com/example/demo_case_crm/service/implementation/UserServiceImpl.java
+++ backend/src/main/java/com/example/demo_case_crm/service/implementation/UserServiceImpl.java
@@ -1,18 +1,24 @@
-package com.example.demo_case_crm.service;
+package com.example.demo_case_crm.service.implementation;
 
+import com.example.demo_case_crm.dto.ClientDTO;
 import com.example.demo_case_crm.dto.UserDTO;
+import com.example.demo_case_crm.entity.Client;
 import com.example.demo_case_crm.entity.User;
 import com.example.demo_case_crm.exception.ResourceNotFoundException;
+import com.example.demo_case_crm.mapper.ClientMapper;
 import com.example.demo_case_crm.mapper.UserMapper;
 import com.example.demo_case_crm.repository.UserRepository;
+import com.example.demo_case_crm.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
 
@@ -33,6 +39,16 @@ public class UserServiceImpl implements UserService{
         User user = userRepository.findById(id).orElseThrow(
                 () -> new ResourceNotFoundException("User","id",Integer.toString(id)));
         return UserMapper.toDTO(user, new UserDTO());
+    }
+
+    @Override
+    public void update(UserDTO userDTO) {
+           User user = userRepository.findById(userDTO.getId()).orElseThrow(
+                    () -> new ResourceNotFoundException("User", "id", Integer.toString(userDTO.getId())));
+
+            UserMapper.toEntity(userDTO, user);
+            user.setUpdatedAt(LocalDateTime.now());
+            userRepository.save(user);
     }
 
 }
